ce451433fbb20058dc2ae1f286efc29e8e479d64
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2018-09-10 21:30:05 +1200

    Initial versions of uputronics RPI+ expansion shield test harness programs

--- UputronicsRPIPlusLed/StartupTask.cs
+++ UputronicsRPIPlusLed/StartupTask.cs
@@ -1,25 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
-
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
-
-namespace UputronicsRPIPlusLed
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) September 2018, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.Rfm9x.UputronicsRPIPlusLed
 {
-    public sealed class StartupTask : IBackgroundTask
-    {
-        public void Run(IBackgroundTaskInstance taskInstance)
-        {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
-        }
-    }
+	using System;
+	using System.Threading;
+	using Windows.ApplicationModel.Background;
+	using Windows.Devices.Gpio;
+
+	public sealed class StartupTask : IBackgroundTask
+	{
+		public void Run(IBackgroundTaskInstance taskInstance)
+		{
+			GpioController gpioController = GpioController.GetDefault();
+
+			GpioPin ce01LedPin = gpioController.OpenPin(5);
+			ce01LedPin.SetDriveMode(GpioPinDriveMode.Output);
+			ce01LedPin.Write(GpioPinValue.Low);
+
+			GpioPin ceo2LedPin = gpioController.OpenPin(21);
+			ceo2LedPin.SetDriveMode(GpioPinDriveMode.Output);
+			ceo2LedPin.Write(GpioPinValue.High);
+
+			GpioPin lanLedPin = gpioController.OpenPin(6);
+			lanLedPin.SetDriveMode(GpioPinDriveMode.Output);
+			lanLedPin.Write(GpioPinValue.Low);
+
+			GpioPin internetLedPin = gpioController.OpenPin(13);
+			internetLedPin.SetDriveMode(GpioPinDriveMode.Output);
+			internetLedPin.Write(GpioPinValue.High);
+
+			while (true)
+			{
+				if (ce01LedPin.Read() == GpioPinValue.High)
+				{
+					ce01LedPin.Write(GpioPinValue.Low);
+				}
+				else
+				{
+					ce01LedPin.Write(GpioPinValue.High);
+				}
+
+				if (ceo2LedPin.Read() == GpioPinValue.High)
+				{
+					ceo2LedPin.Write(GpioPinValue.Low);
+				}
+				else
+				{
+					ceo2LedPin.Write(GpioPinValue.High);
+				}
+
+				if (lanLedPin.Read() == GpioPinValue.High)
+				{
+					lanLedPin.Write(GpioPinValue.Low);
+				}
+				else
+				{
+					lanLedPin.Write(GpioPinValue.High);
+				}
+
+				if (internetLedPin.Read() == GpioPinValue.High)
+				{
+					internetLedPin.Write(GpioPinValue.Low);
+				}
+				else
+				{
+					internetLedPin.Write(GpioPinValue.High);
+				}
+
+				Thread.Sleep(500);
+			}
+		}
+	}
 }
+
--- UputronicsRPIPlusSPI/StartupTask.cs
+++ UputronicsRPIPlusSPI/StartupTask.cs
@@ -1,25 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
-
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
-
-namespace UputronicsRPIPlusSPI
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) September 2018, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.Rfm9x.UputronicsRPIPlusSPI
 {
-    public sealed class StartupTask : IBackgroundTask
-    {
-        public void Run(IBackgroundTaskInstance taskInstance)
-        {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
-        }
-    }
+	using System;
+	using System.Diagnostics;
+	using System.Threading;
+	using Windows.ApplicationModel.Background;
+	using Windows.Devices.Spi;
+
+	public sealed class StartupTask : IBackgroundTask
+	{
+		public void Run(IBackgroundTaskInstance taskInstance)
+		{
+#if CS0
+			const int chipSelectPinNumber = 0;
+#endif
+#if CS1
+			const int chipSelectPinNumber = 1;
+#endif
+			SpiController spiController = SpiController.GetDefaultAsync().AsTask().GetAwaiter().GetResult();
+			var settings = new SpiConnectionSettings(chipSelectPinNumber)
+			{
+				ClockFrequency = 500000,
+				Mode = SpiMode.Mode0,   // From SemTech docs pg 80 CPOL=0, CPHA=0
+			};
+			SpiDevice Device = spiController.GetDevice(settings);
+
+			while (true)
+			{
+				byte[] writeBuffer = new byte[] { 0x42 }; // RegVersion
+				byte[] readBuffer = new byte[1];
+
+				// Read the RegVersion silicon ID to check SPI works
+				Device.TransferSequential(writeBuffer, readBuffer);
+
+#if CS0
+				Debug.WriteLine("CS0 Register RegVer 0x{0:x2} - Value 0X{1:x2} - Bits {2}", writeBuffer[0], readBuffer[0], Convert.ToString(readBuffer[0], 2).PadLeft(8, '0'));
+#endif
+#if CS1
+				Debug.WriteLine("CS1 Register RegVer 0x{0:x2} - Value 0X{1:x2} - Bits {2}", writeBuffer[0], readBuffer[0], Convert.ToString(readBuffer[0], 2).PadLeft(8, '0'));
+#endif
+
+				Thread.Sleep(10000);
+			}
+		}
+	}
 }
